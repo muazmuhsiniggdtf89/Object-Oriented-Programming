9b00f618bb19b8767e38602cd0b67b7dac2e1eb4
muazmuhsiniggdtf89 <mohibmodmr@gmail.com>
2025-08-30 20:58:40 +0600

    Create Point Function 2.py

---
+++ Point Function 2.py
@@ -0,0 +1,15 @@
+class Base:
+    def __init__(self):
+        self.a = "GeeksforGeeks"
+        self._c = "GeeksforGeeks"
+
+class Derived(Base):
+    def __init__(self):
+       
+
+       Base.__init__(self)
+       print("Calling private member of base class: ")
+       print(self.__c)
+
+obj1 = Base()
+print(obj1.a)
